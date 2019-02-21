70d483c0b908c2ae4e65df946de9aa73684e5c15
Andrey Popov <rainfinder@gmail.com>
2019-02-22 00:42:08 +0100

    Switch to C++17 and update environment

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 # Consistent LCG environment (http://lcginfo.cern.ch)
-. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_94a x86_64-slc6-gcc62-opt
+. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_95 x86_64-slc6-gcc8-opt
 
 export HZZ2L2NU_BASE=$(pwd)
 export PATH=$HZZ2L2NU_BASE/bin:$PATH

1d339ea0786e01b6acffc72e64522d0f21f3ae89
Andrey Popov <rainfinder@gmail.com>
2019-12-11 12:23:36 +0100

    Introduce common interface for event weights
    Migrate PileUpWeight to the new interface.

--- include/PileUpWeight.h
+++ include/PileUpWeight.h
@@ -1,6 +1,9 @@
-#ifndef PILEUPWEIGHT_H_
-#define PILEUPWEIGHT_H_
+#ifndef HZZ2L2NU_INCLUDE_PILEUPWEIGHT_H_
+#define HZZ2L2NU_INCLUDE_PILEUPWEIGHT_H_
 
+#include <WeightBase.h>
+
+#include <array>
 #include <filesystem>
 #include <memory>
 #include <string>
@@ -9,6 +12,7 @@
 #include <TTreeReaderValue.h>
 
 #include <Dataset.h>
+#include <EventCache.h>
 #include <Options.h>
 
 
@@ -19,15 +23,37 @@
  * and the target profile in data are read from the analysis configuration file.
  * Systematic uncertainty in pileup is implemented using alternative data
  * profiles, which have been constructed assuming different values for the
- * pileup cross section.
+ * pileup cross section. Computed weights are cached on per-event basis.
  */
-class PileUpWeight {
+class PileUpWeight : public WeightBase {
  public:
   /// Constructor
   PileUpWeight(Dataset &dataset, Options const &options);
 
+  virtual double NominalWeight() const override {
+    if (cache_.IsUpdated())
+      Update();
+    return weights_[0];
+  }
+
+  virtual int NumVariations() const override {
+    return 2;
+  }
+
   /// Computes the pileup weight for the current event
-  double operator()() const;
+  virtual double operator()() const override {
+    if (cache_.IsUpdated())
+      Update();
+    return weights_[defaultWeightIndex_];
+  }
+
+  virtual double RelWeight(int variation) const override {
+    if (cache_.IsUpdated())
+      Update();
+    return weights_[variation + 1] / weights_[0];
+  }
+
+  virtual std::string_view VariationName(int variation) const override;
 
  private:
   /**
@@ -39,13 +65,36 @@ class PileUpWeight {
   static TH1 *ReadHistogram(std::filesystem::path const &path,
                             std::string const &name);
 
+  /// Computes all weights for the current event
+  void Update() const;
+
+  ///@{
   /**
    * \brief Histograms representing pileup profiles in data and simulation
    *
    * They are normalized to represent probability density. Under- and overflow
-   * bins are empty.
+   * bins are empty. The profiles in data are given in the order nominal, up,
+   * down.
    */
-  std::unique_ptr<TH1> dataProfile, simProfile;
+  std::array<std::unique_ptr<TH1>, 3> dataProfiles_;
+  std::unique_ptr<TH1> simProfile_;
+  ///@}
+
+  EventCache cache_;
+
+  /**
+   * \brief Cached weights
+   *
+   * They are given in the order nominal, up, down.
+   */
+  mutable std::array<double, 3> weights_;
+
+  /**
+   * \brief Index of the default weight to be returned by operator()
+   *
+   * Corresponds to array \ref weights_.
+   */
+  int defaultWeightIndex_;
   
   /**
    * \brief Interface to read the expected number of pileup interactions
@@ -53,5 +102,5 @@ class PileUpWeight {
   mutable TTreeReaderValue<float> mu_;
 };
 
-#endif  // PILEUPWEIGHT_H_
+#endif  // HZZ2L2NU_INCLUDE_PILEUPWEIGHT_H_
 

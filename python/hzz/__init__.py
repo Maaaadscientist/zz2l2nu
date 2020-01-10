c6fecd26209cf8b9f8fd603b551b127062d71d4f
Andrey Popov <rainfinder@gmail.com>
2020-01-10 15:56:03 +0100

    Add script to plot systematic uncertainties

--- python/hzz/__init__.py
+++ python/hzz/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import Dataset, parse_datasets_file
 from .pyroothist.pyroothist import Hist1D
-from .util import SystDatasetSelector
+from .util import SystDatasetSelector, mpl_style
 

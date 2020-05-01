fa014c67a1ecda78216d46c9b107b820e22d0757
Mostafa Mahdavikhorrami <mahdavi.mst@gmail.com>
2020-05-02 01:29:33 +0200

    Read MET Filters from master configuration file
    This also fixes issue #47

--- src/MetFilters.cc
+++ src/MetFilters.cc
@@ -1,17 +1,17 @@
 #include <MetFilters.h>
 
 
-MetFilters::MetFilters(Dataset &dataset) {
-  // Select flags to check as recommended in [1]
-  // [1] https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2?rev=137#2016_data
-  for (auto const &flagName: {
-      "Flag_goodVertices", "Flag_globalSuperTightHalo2016Filter",
-      "Flag_HBHENoiseFilter", "Flag_HBHENoiseIsoFilter",
-      "Flag_EcalDeadCellTriggerPrimitiveFilter", "Flag_BadPFMuonFilter"})
-    flags_.emplace_back(dataset.Reader(), flagName);
+MetFilters::MetFilters(Options const &options, Dataset &dataset) {
+  std::vector<std::string> flagsName;
+  if (dataset.Info().IsSimulation())
+    flagsName = Options::NodeAs<std::vector<std::string>>(
+        options.GetConfig(), {"met_filters", "sim"});
+  else
+    flagsName = Options::NodeAs<std::vector<std::string>>(
+        options.GetConfig(), {"met_filters", "data"});
 
-  if (not dataset.Info().IsSimulation())
-    flags_.emplace_back(dataset.Reader(), "Flag_eeBadScFilter");
+  for (auto const &flagName : flagsName)
+    flags_.emplace_back(dataset.Reader(), flagName.c_str());
 }
 
 

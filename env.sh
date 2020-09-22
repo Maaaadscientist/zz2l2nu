d48a8c280c8fe87b68293f463336abd4ab58733a
Andrey Popov <rainfinder@gmail.com>
2020-09-22 13:42:57 +0200

    Switch to EL7 by default
    Set up the environment depending on the Linux release, so it's still
    possible to use EL6. This works also in Singularity containers. The
    check for substring is from this answer [1] and should work in all POSIX
    shells.
    [1] https://stackoverflow.com/a/8811800/966461

--- env.sh
+++ env.sh
@@ -1,7 +1,13 @@
 # Consistent LCG environment (http://lcginfo.cern.ch)
-. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_97python3 x86_64-slc6-gcc8-opt
+release=`cat /etc/redhat-release`
+if test "${release#*release 6.}" != "$release"; then
+  # Scientific Linux 6
+  . /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_97python3 x86_64-slc6-gcc8-opt
+else
+  # CentOS 7
+  . /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_97python3 x86_64-centos7-gcc9-opt
+fi
 
 export HZZ2L2NU_BASE=$(pwd)
 export PYTHONPATH=$HZZ2L2NU_BASE/python:$PYTHONPATH
 export PATH=$HZZ2L2NU_BASE/bin:$PATH
-

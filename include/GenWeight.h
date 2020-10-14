d111b01149e1ecd01630b9468218321dfda3bdb4
Andrey Popov <rainfinder@gmail.com>
2020-10-14 13:39:15 +0200

    Implement PDF variations
    Individual PDF variations are combined on per-event basis. With Hessian
    variations, they are added up in quadrature; with MC replicas, the
    standard deviation of individual weights in each event is used.
    
    Also implement alpha_s variation. When needed, it is rescaled to the
    recommended variation of +-1.5e-3. This fixes shears#37.

--- include/GenWeight.h
+++ include/GenWeight.h
@@ -1,7 +1,10 @@
 #ifndef HZZ2L2NU_INCLUDE_GENWEIGHT_H_
 #define HZZ2L2NU_INCLUDE_GENWEIGHT_H_
 
+#include <array>
 #include <map>
+#include <optional>
+#include <tuple>
 #include <utility>
 
 #include <TTreeReaderArray.h>
@@ -57,7 +60,7 @@ public:
   double NominalWeight() const override;
 
   int NumVariations() const override {
-    return (lheScaleWeightsPresent_) ? 4 : 0;
+    return availableVariations_.size();
   }
 
   double operator()() const override {
@@ -73,28 +76,49 @@ public:
    * The order of the variations, provided that all are available, is as
    * follows:
    *   -# ME renormalization scale up and down,
-   *   -# Factorization scale up and down.
+   *   -# Factorization scale up and down,
+   *   -# Combined PDF up and down,
+   *   -# alpha_s in PDF up and down.
    */
   double RelWeight(int variation) const override;
 
-  /// Returns relative weight for requested variation in alpha_s in PDF
+  /**
+   * \brief Returns relative weight for requested variation in alpha_s in PDF
+   *
+   * The variation is scaled linearly to the recommended size.
+   */
   double RelWeightAlphaS(Var direction) const;
 
   /// Returns relative weight for requested variation in ME scales
   double RelWeightMEScale(Var renorm, Var factor) const;
 
   /**
-   * \brief Returns relative weight for given PDF replica
+   * \brief Returns relative weight for combined PDF variation.
    *
-   * It is expected but not checked that the provided replica index satisfies
-   * 0 <= replica < 100.
+   * This is a pure PDF variation, i.e. it does not include alpha_s. Different
+   * weights are combined on the fly on the per-even basis, as described
+   * <a href="https://indico.cern.ch/event/961210/#199-pdf-variations">here</a>.
+   * The resulting variation is conservative.
    */
-  double RelWeightPdf(int replica) const;
+  double RelWeightPdf(Var direction) const;
 
-  std::string_view VariationName(int variation) const override;
+  std::string_view VariationName(int variation) const override {
+    return availableVariations_.at(variation);
+  }
 
 private:
+  /// Type of PDF variations
+  enum class PdfVarType {
+    Hessian,  ///< Hessian variations
+    MC  ///< MC replicas
+  };
+
   void InitializeLheScale(Dataset &dataset);
+  void InitializePdf(Dataset &dataset);
+
+  /// Helper function to look up metadata about a PDF set from YAML file
+  static std::tuple<PdfVarType, std::optional<std::array<int, 2>>, double>
+  LookUpPdfSet(int lhapdf);
 
   /**
    * \brief Common dataset weight, in pb
@@ -107,14 +131,45 @@ private:
   /// Indicates whether LHE scale variations are available
   bool lheScaleWeightsPresent_;
 
- /**
-  * \brief Indicates if the sample has bugged LHE weights (currently Pt-binned
-  * WG)
-  *
-  * Problem seems to happen during translation from MiniAOD to NanoAOD: see
-  * https://github.com/cms-nanoAOD/cmssw/issues/520
-  */
-  bool hasBuggedLheWeights_;
+  /**
+   * \brief Mapping from variations in the renormalization and factorization
+   * scales in the ME to the index of the corresponding weight in the source
+   * array.
+   */
+  std::map<std::pair<Var, Var>, int> meScaleIndices_;
+
+  /// Indicates whether PDF weights are available
+  bool pdfWeightsPresent_;
+
+  /**
+   * \brief Indices for PDF weights with variations
+   *
+   * These indices correspond to the weights in branch LHEPdfWeight. They don't
+   * include the nominal weight nor the alpha_s variations. The range of indices
+   * to be used is [first, second), i.e. semiinclusive.
+   */
+  std::pair<int, int> pdfWeightsIndices_;
+
+  /// Type of PDF variations
+  PdfVarType pdfVarType_;
+
+  /// Indicates whether PDF weights include also alpha_s variations
+  bool alphaSWeightsPresent_;
+
+  /**
+   * \brief Indices of weights for alpha_s variation
+   *
+   * Correspond to the weights in branch LHEPdfWeight. The first index is for
+   * an increase in alpha_s, the second one is for a decrease. The array is not
+   * initialized if alphaSWeightsPresent_ is false.
+   */
+  std::array<int, 2> alphaSWeightsIndices_;
+
+  /// How much to rescale alpha_s variation given by weights in input file
+  double alphaSVarScaleFactor_;
+
+  /// Names of available variations
+  std::vector<std::string> availableVariations_;
 
   /**
    * \brief Default variation index to be used with \ref operator()
@@ -124,17 +179,10 @@ private:
    */
   int defaultVariationIndex_;
 
-  /**
-   * \brief Mapping from variations in the renormalization and factorization
-   * scales in the ME to the index of the corresponding weight in the source
-   * array.
-   */
-  std::map<std::pair<Var, Var>, int> meScaleIndices_;
-
   mutable TTreeReaderValue<float> srcLheNominalWeight_;
   mutable TTreeReaderValue<float> srcGenNominalWeight_;
-  mutable TTreeReaderArray<float> srcPdfWeights_;
   mutable TTreeReaderArray<float> srcScaleWeights_;
+  mutable TTreeReaderArray<float> srcPdfWeights_;
 };
 
 #endif  // HZZ2L2NU_INCLUDE_GENWEIGHT_H_
--- src/GenWeight.cc
+++ src/GenWeight.cc
@@ -2,33 +2,49 @@
 
 #include <algorithm>
 #include <initializer_list>
+#include <regex>
+#include <string>
 
+#include <yaml-cpp/yaml.h>
+
+#include <FileInPath.h>
 #include <HZZException.h>
+#include <Logger.h>
 
 
 GenWeight::GenWeight(Dataset &dataset, Options const &options)
   : srcLheNominalWeight_{dataset.Reader(), "LHEWeight_originalXWGTUP"},
     srcGenNominalWeight_{dataset.Reader(), "Generator_weight"},
-    srcPdfWeights_{dataset.Reader(), "LHEPdfWeight"},
-    srcScaleWeights_{dataset.Reader(), "LHEScaleWeight"} {
+    srcScaleWeights_{dataset.Reader(), "LHEScaleWeight"},
+    srcPdfWeights_{dataset.Reader(), "LHEPdfWeight"} {
 
   DatasetInfo const &info = dataset.Info();
   datasetWeight_ = info.CrossSection()
       / (info.NumEventsTotal() * info.MeanWeight());
 
-  auto const systLabel = options.GetAs<std::string>("syst");
-  if (systLabel == "me_renorm_up")
-    defaultVariationIndex_ = (lheScaleWeightsPresent_) ? 0 : -1;
-  else if (systLabel == "me_renorm_down")
-    defaultVariationIndex_ = (lheScaleWeightsPresent_) ? 1 : -1;
-  else if (systLabel == "factor_up")
-    defaultVariationIndex_ = (lheScaleWeightsPresent_) ? 2 : -1;
-  else if (systLabel == "factor_down")
-    defaultVariationIndex_ = (lheScaleWeightsPresent_) ? 3 : -1;
-  else
-    defaultVariationIndex_ = -1;
-
   InitializeLheScale(dataset);
+  InitializePdf(dataset);
+
+  if (lheScaleWeightsPresent_)
+    for (auto const &name : {
+        "me_renorm_up", "me_renorm_down", "factor_up", "factor_down"})
+      availableVariations_.emplace_back(name);
+  if (pdfWeightsPresent_) {
+    availableVariations_.emplace_back("pdf_up");
+    availableVariations_.emplace_back("pdf_down");
+  }
+  if (alphaSWeightsPresent_) {
+    availableVariations_.emplace_back("alphaS_up");
+    availableVariations_.emplace_back("alphaS_down");
+  }
+
+  auto const systLabel = options.GetAs<std::string>("syst");
+  auto const r = std::find(
+      availableVariations_.begin(), availableVariations_.end(), systLabel);
+  if (r == availableVariations_.end())
+    defaultVariationIndex_ = -1;
+  else
+    defaultVariationIndex_ = r - availableVariations_.begin();
 }
 
 
@@ -59,88 +75,120 @@ double GenWeight::NominalWeight() const {
 
 
 double GenWeight::RelWeight(int variation) const {
-  switch (variation) {
-    case 0:
-      return RelWeightMEScale(Var::Up, Var::Nominal);
-    case 1:
-      return RelWeightMEScale(Var::Down, Var::Nominal);
-    case 2:
-      return RelWeightMEScale(Var::Nominal, Var::Up);
-    case 3:
-      return RelWeightMEScale(Var::Nominal, Var::Down);
-    default:
-      throw HZZException{"Illegal index."};
+  enum class Group {
+    None,
+    MEScale,
+    PDF,
+    AlphaS
+  };
+
+  Group group = Group::None;
+  int index = variation;
+  for (auto const &[g, offset, present] : {
+      std::make_tuple(Group::MEScale, 4, lheScaleWeightsPresent_),
+      std::make_tuple(Group::PDF, 2, pdfWeightsPresent_),
+      std::make_tuple(Group::AlphaS, 2, alphaSWeightsPresent_)}) {
+    if (not present)
+      continue;
+    if (index < offset) {
+      group = g;
+      break;
+    } else {
+      index -= offset;
+    }
   }
+  if (group == Group::None)
+    throw HZZException{"Illegal variation index."};
+
+  if (group == Group::MEScale) {
+    switch (index) {
+      case 0:
+        return RelWeightMEScale(Var::Up, Var::Nominal);
+      case 1:
+        return RelWeightMEScale(Var::Down, Var::Nominal);
+      case 2:
+        return RelWeightMEScale(Var::Nominal, Var::Up);
+      case 3:
+        return RelWeightMEScale(Var::Nominal, Var::Down);
+    }
+  } else if (group == Group::PDF) {
+    switch (index) {
+      case 0:
+        return RelWeightPdf(Var::Up);
+      case 1:
+        return RelWeightPdf(Var::Down);
+    }
+  } else if (group == Group::AlphaS) {
+    switch (index) {
+      case 0:
+        return RelWeightAlphaS(Var::Up);
+      case 1:
+        return RelWeightAlphaS(Var::Down);
+    }
+  }
+
+  // Should never reach this point
+  throw HZZException{"Illegal variation index."};
 }
 
 
-double GenWeight::RelWeightAlphaS(Var) const {
-  // In POWHEG gg->ZZ data set [1], LHE weights with indices 109 and 110
-  // correspond to PDF sets 265000 (NNPDF30_nlo_as_0117) and 266000
-  // (NNPDF30_nlo_as_0119), which thus define the alpha_s variation in PDF. In
-  // the stored vector, these weights are found at indices 110 and 111. However,
-  // it is not guaranteed that these weights have the same meaning for all
-  // samples.
-  // [1] /ZZTo2L2Nu_13TeV_powheg_pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2/MINIAODSIM
-
-  /*
-  if (srcWeights_.GetSize() < 112) {
-    HZZException exception;
-    exception << "Cannot access alpha_s variation in PDF (weights with indices "
-      "110 and 111) because only " << srcWeights_.GetSize() << " weights are "
-      "available.";
-    throw exception;
-  }
-
-  if (direction == Var::Up)
-    return srcWeights_[111] / srcWeights_[1];
-  else if (direction == Var::Down)
-    return srcWeights_[110] / srcWeights_[1];
-  else
+double GenWeight::RelWeightAlphaS(Var direction) const {
+  if (not alphaSWeightsPresent_ or direction == Var::Nominal)
     return 1.;
-  */
-  return 1.; // No alpha_S variation in NanoAOD.
+
+  double weight;
+  if (direction == Var::Up)
+    weight = srcPdfWeights_[alphaSWeightsIndices_[0]];
+  else
+    weight = srcPdfWeights_[alphaSWeightsIndices_[1]];
+  double const scaledWeight = 1. + (weight - 1.) * alphaSVarScaleFactor_;
+  return scaledWeight;
 }
 
 
 double GenWeight::RelWeightMEScale(Var renorm, Var factor) const {
-  if (hasBuggedLheWeights_) return 1.;
+  if (not lheScaleWeightsPresent_)
+    return 1.;
   if (srcScaleWeights_.GetSize() < 9) {
     HZZException exception;
-    exception << "Cannot access ME scale variations (weights with indices 0 to 8) "
-      "because only " << srcScaleWeights_.GetSize() << " weights are available.";
+    exception << "Cannot access ME scale variations (weights with indices 0 "
+        << "to 8) because only " << srcScaleWeights_.GetSize()
+        << " weights are available.";
     throw exception;
   }
-
-  return srcScaleWeights_[meScaleIndices_.at({renorm, factor})] / srcScaleWeights_[4];
+  double const weight = srcScaleWeights_[meScaleIndices_.at({renorm, factor})];
+  return weight / srcScaleWeights_[4];
 }
 
 
-double GenWeight::RelWeightPdf(int replica) const {
-  if (srcPdfWeights_.GetSize() < 100) {
-    HZZException exception;
-    exception << "Cannot access PDF variations (weights with indices 0 to 99) "
-      "because only " << srcPdfWeights_.GetSize() << " weights are available.";
-    throw exception;
+double GenWeight::RelWeightPdf(Var direction) const {
+  if (not pdfWeightsPresent_ or direction == Var::Nominal)
+    return 1.;
+
+  double sumW = 0., sumW2 = 0.;
+  for (int i = pdfWeightsIndices_.first; i < pdfWeightsIndices_.second; ++i) {
+    double const w = srcPdfWeights_[i];
+    sumW += w;
+    sumW2 += w * w;
   }
+  int const n = pdfWeightsIndices_.second - pdfWeightsIndices_.first;
+  double const meanWeight = sumW / n;
+  double const sumDiff2 = sumW2 - n * std::pow(meanWeight, 2);
 
-  return srcPdfWeights_[replica]; // Relative wrt nominal weight
-}
+  double deltaWeight;
+  if (pdfVarType_ == PdfVarType::Hessian)
+    // Deviations from mean weight are summed up in quadrature
+    deltaWeight = std::sqrt(sumDiff2);
+  else
+    // Standard deviation of the weights
+    deltaWeight = std::sqrt(sumDiff2 / (n - 1));
 
-
-std::string_view GenWeight::VariationName(int variation) const {
-  switch (variation) {
-    case 0:
-      return "me_renorm_up";
-    case 1:
-      return "me_renorm_down";
-    case 2:
-      return "factor_up";
-    case 3:
-      return "factor_down";
-    default:
-      return "";
-  }
+  // When applying the variation, assume that the mean weight is 1. In practice,
+  // it's very close to 1.
+  if (direction == Var::Up)
+    return 1. + deltaWeight;
+  else
+    return 1. - deltaWeight;
 }
 
 
@@ -152,11 +200,11 @@ void GenWeight::InitializeLheScale(Dataset &dataset) {
       lheScaleWeightsPresent_ = lheScale.as<bool>();
   }
 
-  hasBuggedLheWeights_ = false;
-  // Drop WG samples of 2017/2018 as they have one fewer weight.
+  // Drop WG samples of 2017/2018 as they have one fewer weight. See
+  // https://github.com/cms-nanoAOD/cmssw/issues/520
   auto const WGNode = dataset.Info().Parameters()["wgamma_lnugamma"];
   if (WGNode and not WGNode.IsNull())  // Also drop the LO case for consistency
-    hasBuggedLheWeights_ = true;
+    lheScaleWeightsPresent_ = false;
 
   // Set up the mapping between the ME scale variations and indices in the
   // vector of weights. The convention for the weights is extracted from [1].
@@ -170,4 +218,121 @@ void GenWeight::InitializeLheScale(Dataset &dataset) {
   meScaleIndices_[{Var::Up, Var::Down}] = 6;
   meScaleIndices_[{Var::Up, Var::Nominal}] = 7;
   meScaleIndices_[{Var::Up, Var::Up}] = 8;
+}
+
+
+void GenWeight::InitializePdf(Dataset &dataset) {
+  std::string const pdfBranchTitle{
+      dataset.Reader().GetTree()->GetBranch("LHEPdfWeight")->GetTitle()};
+  if (pdfBranchTitle.empty()) {
+    LOG_WARN << "Weights for PDF variations are not found.";
+    pdfWeightsPresent_ = false;
+    alphaSWeightsPresent_ = false;
+    return;
+  }
+  pdfWeightsPresent_ = true;
+
+  // Extract the range of LHAPDF IDs of stored PDF weights from the title of
+  // the branch
+  std::regex pattern{R"(LHA\s+IDs\s+(\d+)\s*-\s*(\d+)\b)"};
+  std::smatch match;
+  if (not std::regex_search(pdfBranchTitle, match, pattern)) {
+    HZZException exception;
+    exception << "Failed to parse title of branch with PDF weights \""
+        << pdfBranchTitle << "\".";
+    throw exception;
+  }
+  int const lhapdfIdFirst = std::stoi(match[1]);
+  int const lhapdfIdLast = std::stoi(match[2]);
+
+  int lhapdfId;  // ID of the PDF set as a whole
+  bool nominalWeightPresent;
+  if (lhapdfIdFirst % 100 == 1) {
+    nominalWeightPresent = false;
+    lhapdfId = lhapdfIdFirst - 1;
+  } else {
+    nominalWeightPresent = true;
+    lhapdfId = lhapdfIdFirst;
+  }
+
+  auto const &[pdfVarType, alphaSVarIndices, alphaSVarSize]
+      = LookUpPdfSet(lhapdfId);
+  alphaSWeightsPresent_ = bool(alphaSVarIndices);
+
+  pdfWeightsIndices_.first = (nominalWeightPresent) ? 1 : 0;
+  pdfWeightsIndices_.second = lhapdfIdLast - lhapdfIdFirst + 1;
+  if (alphaSWeightsPresent_)
+    pdfWeightsIndices_.second -= 2;  // Last two weights are for alpha_s
+  pdfVarType_ = pdfVarType;
+
+  if (alphaSWeightsPresent_) {
+    alphaSWeightsIndices_ = alphaSVarIndices.value();
+    // PDF4LHC15 recommends a variation of 1.5e-3
+    alphaSVarScaleFactor_ = 1.5e-3 / alphaSVarSize;
+  }
+
+  LOG_DEBUG << "Found PDF weights for LHAPDF IDs " << lhapdfIdFirst << " to "
+      << lhapdfIdLast << ", which " << ((nominalWeightPresent) ? "" : "don't ")
+      << "include nominal weight.";
+  LOG_DEBUG << "Type of PDF variation: \""
+      << ((pdfVarType_ == PdfVarType::Hessian) ? "Hessian" : "MC") << "\".";
+  if (alphaSWeightsPresent_)
+    LOG_DEBUG << "Variation in alpha_s is present. It will be scaled by "
+        << "factor " << alphaSVarScaleFactor_ << ".";
+  else
+    LOG_DEBUG << "Variation in alpha_s is missing.";
+}
+
+
+std::tuple<GenWeight::PdfVarType, std::optional<std::array<int, 2>>, double>
+GenWeight::LookUpPdfSet(int lhapdf) {
+  YAML::Node pdfInfos = YAML::LoadFile(FileInPath::Resolve("pdf.yaml"));
+  auto pdfInfo = pdfInfos[lhapdf];
+  if (not pdfInfo) {
+    HZZException exception;
+    exception << "Found no metadata for PDF set with LHAPDF ID "
+        << lhapdf << ".";
+    throw exception;
+  }
+
+  for (auto const &field : {
+      "pdf_variations_type", "alpha_s_indices", "alpha_s_variation"})
+    if (auto node = pdfInfo[field]; not node) {
+      HZZException exception;
+      exception << "Entry with metadata for PDF set " << lhapdf
+          << " doesn't contain mandatory field \"" << field << "\".";
+      throw exception;
+    }
+
+  PdfVarType pdfVarType;
+  auto const s = pdfInfo["pdf_variations_type"].as<std::string>();
+  if (s == "Hessian") {
+    pdfVarType = PdfVarType::Hessian;
+  } else if (s == "MC") {
+    pdfVarType = PdfVarType::MC;
+  } else {
+    HZZException exception;
+    exception << "Value \"" << s << "\" for \"pdf_variations_type\" in "
+        << "metadata for PDF set " << lhapdf << " is not supported.";
+    throw exception;
+  }
+
+  std::optional<std::array<int, 2>> alphaSVarIndices;
+  if (auto node = pdfInfo["alpha_s_indices"]; not node.IsNull()) {
+    if (not node.IsSequence() or node.size() != 2) {
+      HZZException exception;
+      exception << "Value for \"alpha_s_indices\" in metadata for PDF set "
+          << lhapdf << " is not an array of size 2.";
+      throw exception;
+    }
+    alphaSVarIndices.emplace();
+    (*alphaSVarIndices)[0] = node[0].as<int>();
+    (*alphaSVarIndices)[1] = node[1].as<int>();
+  }
+
+  double alphaSVarSize = 0.;
+  if (auto node = pdfInfo["alpha_s_variation"]; not node.IsNull())
+    alphaSVarSize = node.as<double>();
+
+  return {pdfVarType, alphaSVarIndices, alphaSVarSize};
 }

cdcdbac07add6575f0ce449bad05625b301daf3d
Hanwen Wang <wanghanwen@ihep.ac.cn>
2024-05-06 10:24:01 +0000

    update maximum CPU use

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=parallel_job
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
+#SBATCH --ntasks=48
 #SBATCH --cpus-per-task=1
 #SBATCH --time=06:00:00
 #SBATCH --output=/dev/null
